08ac4cfaf971daf62a70e1ad56d6e9b9118666a6
Finton Prosser <finprosser@gmail.com>
2022-02-09 17:15:04 -0500

    Finished Diagonal_difference

--- Problems/Diagonal_Difference.java
+++ Problems/Diagonal_Difference.java
@@ -1,5 +1,12 @@
 package Problems;
 
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+
+
+
 /* 
 
 Given a square matrix, calculate the absolute difference between the sums of its diagonals.
@@ -16,7 +23,45 @@ Their absolute difference is |15-17| = 2
  */
 
 public class Diagonal_Difference {
+
+    public static int diagonalDifference(List<List<Integer>> arr){
+        
+        int r_to_l = 0;
+        int l_to_r = 0;
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int l = arr.size() - 1;
+        
+        int rows = arr.size();
+        int columns = arr.get(0).size();
+        
+        while(i < rows && j < columns && k < rows && l >= 0) {
+            r_to_l += arr.get(i).get(j);
+            l_to_r += arr.get(k).get(l);
+            
+            
+            i++;
+            j++;
+            k++;
+            l--;
+            
+        }
+        
+        return Math.abs(r_to_l - l_to_r);
+        
+        
+    
+
+    }
+
+
+
     public static void main(String[] args){
+        List<List<Integer>> arr = List.of(List.of(11,2,4), List.of(4,5,6), List.of(10,8,-12));
+        System.out.println(diagonalDifference(arr));
+
+        
         
 
     }
